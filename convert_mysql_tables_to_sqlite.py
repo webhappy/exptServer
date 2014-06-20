79fe69f5ea6326231ddb93578f8ae4eb0dd7fda0
webhappy <dwchen@gmail.com>
2014-06-20 16:10:44 -0700

    small typo

--- convert_mysql_tables_to_sqlite.py
+++ convert_mysql_tables_to_sqlite.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.call('./mysql2sqlite.sh --password=mysql_password CSPR sgRNAs comparisons tickers | sqlite3 local.db',shell=True)
+subprocess.call('./mysql2sqlite.sh --password=mysql_password CRISPR sgRNAs comparisons tickers | sqlite3 local.db',shell=True)
